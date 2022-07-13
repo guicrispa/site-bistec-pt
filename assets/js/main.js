68e6952dcb9a469f58cf78d930f27ae96ccfc7f4
MateusBistec <mateusbistec@gmail.com>
2022-07-13 15:03:05 -0300

    atualiza modal, js e html. Falta fechar ao clicar no x

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,52 +15,14 @@ function menuResponsive(){
 }
 
 //Funções do Pop-up -----------------------------------------------------------------
-var modal = document.getElementById("myModal");
-
-var btn = document.getElementById("myBtn");
-
-var span = document.getElementsByClassName("close")[0];
-
-var modal_content = document.getElementsByClassName("modal-content")
-
-btn.onclick = function() {
-modal.style.display = "block";
-}
-
-modal.onclick = function() {
-modal.style.display = "none";
-}
-
-modal_content.onclick = function() {
-modal.style.display = "block";
-}
-
-span.onclick = function() {
-modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-if (event.target == modal) {
-    modal.style.display = "none";
-}
-}
-
-function controlaDialog(idDialog){
-    if(document.getElementById(idDialog).open){
-        fechaDialog(idDialog)
-    }else{
-        abreDialog(idDialog)
-    }       
-}
-
-function fechaDialog(){
-    document.querySelectorAll('dialog').forEach(dia => dia.removeAttribute('open'))
-    document.getElementsByTagName('html')[0].removeAttribute("onclick", "fechaDialog()")
-}
-function abreDialog(idDialog) {
-    fechaDialog()
-    document.getElementById(idDialog).setAttribute("open", "")
+function openModal(idModal, idBtnClose){
+    let modal = document.getElementById(idModal)
+    modal.style.display = "block"
     setTimeout(() => {
-        document.getElementsByTagName('html')[0].setAttribute("onclick", "fechaDialog()")
-    }, "100")
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }, 10);
 }
