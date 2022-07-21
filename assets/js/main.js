5e92c02b6a50181e81b54c748420fdeed0da5aaf
MateusBistec <mateusbistec@gmail.com>
2022-07-21 14:41:49 -0300

    tira onclick do menu mobile do html para o js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,6 +46,7 @@ window.onresize = verificaTela
 window.onload = verificaTela
 
 //abre e fecha menu mobile
+document.getElementsByName('menu-outline')[0].addEventListener('click', menuResponsive)
 function menuResponsive(){
     let menu = document.getElementsByClassName('normal-menu')[0]
     if(menu.style.display == 'inherit'){
