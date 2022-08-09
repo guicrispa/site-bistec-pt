6441a3a77334ad369af30a5b32b4f11df456ceac
MateusBistec <mateusbistec@gmail.com>
2022-08-09 15:01:56 -0300

    alterações no corpo do email e logica entre as paginas de falha e obrigado

--- email/contato.php
+++ email/contato.php
@@ -1,25 +1,25 @@
 <?php
     if(isset($_POST['enviarEmail']) && isset($_POST['emailRemetente']) && !empty($_POST['emailRemetente'])){
-        $nomeRemetente=$_POST['nomeRemetente'];
+        $nomeRemetente= ucwords(strtolower($_POST['nomeRemetente']));
         $assuntoEmail=$_POST['assuntoEmail'];
         $emailRemetente=$_POST['emailRemetente'];
         $conteudoEmail=$_POST['conteudoEmail'];
 
-        $emailDestinatario="mateus@bistec.com";
+        $emailDestinatario="mateus@bistec.com.br";
         $headersR="From: ".$emailRemetente;
         $headersD="From: ".$emailDestinatario;
 
         // Corpo do email enviado para a Bistec
         $txtDestinatario="Você recebeu um email de ".$nomeRemetente.
         ".\n\n".
-        "Email do Remetende: ".$emailRemetente."\n".
-        "Assunto:\n".$conteudoEmail;
+        "Email do Remetende: ".$emailRemetente."\n\n".
+        "Conteúdo:\n".$conteudoEmail;
 
         // Corpo do email enviado para o cliente
-        $txtRemetente="Seu e-mail foi recebido com sucesso, Sr(a) ".$nomeRemetente.
+        $txtRemetente="Seu e-mail foi recebido com sucesso pela equipe da Bistec, Sr(a) ".$nomeRemetente.
         ".\n\n".
-        "Obrigado pelo contato e, caso seja necessário, estaremos entrando em contato assim que possível pelo email: ".$emailRemetente."\n".
-        "Segue o conteúdo que recebemos:\n".$conteudoEmail;
+        "Obrigado pelo contato e, caso seja necessário, estaremos entrando em contato assim que possível pelo email: ".$emailRemetente."\n\n\n".
+        "Segue o conteúdo que recebemos:\n\n".$conteudoEmail;
 
         session_start();
         $_SESSION['emailRemetente']=$emailRemetente;
@@ -27,11 +27,12 @@
 
         // Valida envio dos emails
         if(mail($emailDestinatario, $assuntoEmail, $txtDestinatario, $headersR)
-            && mail($emailRemetente, $assuntoEmail, $txtRemetente, $headersD)
+            && mail($emailRemetente, "Email entregue: ".$assuntoEmail, $txtRemetente, $headersD)
         ){
             header('location: ./obrigado.php');
         }
         else{
+            $_SESSION['falha']=true;
             header('location: ./falha.php');
         } 
     }else{
