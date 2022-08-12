18f3105c2bef2dc583386316cada70e516f02060
MateusBistec <mateusbistec@gmail.com>
2022-08-12 16:43:00 -0300

    adiciona busca pelo caminho para automatizar as demais páginas de subpastas

--- assets/js/componente_menu.js
+++ assets/js/componente_menu.js
@@ -1,30 +1,31 @@
-var menu=document.getElementsByClassName('menu-principal')[0]
+let menu=document.getElementsByClassName('menu-principal')[0]
+let Caminho = document.getElementsByTagName('script')[0].attributes[0].textContent.split('/')[0]+'/'
 
 menu.insertAdjacentHTML('afterend',/*html*/`
     <header class="header-bistec" id="#first_section">
         <a href="#first_section" class="logo-bistec">
-            <img src="assets/images/logo-bistec.png" alt="Logo - Bistec">
+            <img src="${Caminho}assets/images/logo-bistec.png" alt="Logo - Bistec">
         </a>
         <ul class="normal-menu">
             <li>
-                <a href="index.html" class="normal" >HOME</a>
+                <a href="${Caminho}index.html" class="normal" >HOMEoi</a>
             </li>
             <li>
                 <a href="#bistec-automaçao" class="normal">PORTFÓLIO <ion-icon name="chevron-down-outline"></ion-icon></a>
                 <ul class="normal-submenu">
-                    <li><a href="produtos/automacao.html"><ion-icon name="at-sharp"></ion-icon> BISTEC AUTOMAÇÃO</a></li>
-                    <li><a href="produtos/cloud.html"> <ion-icon name="cloud-done-sharp"></ion-icon> BISTEC CLOUD</a></li>
-                    <li><a href="produtos/edu.html"> <ion-icon name="pencil-sharp"></ion-icon> BISTEC EDU</a></li>
-                    <li><a href="produtos/gdpr.html"><ion-icon name="shield-checkmark-sharp"></ion-icon> BISTEC GDPR</a></li>
-                    <li><a href="produtos/msp.html"> <ion-icon name="lock-closed"></ion-icon> BISTEC MSP</a></li>
-                    <li><a href="produtos/outsourcing.html"> <ion-icon name="settings-sharp"></ion-icon> BISTEC OUTSOURCING</a></li>
-                    <li><a href="produtos/renting.html"> <ion-icon name="reload-circle-sharp"></ion-icon> BISTEC RENTING</a></li>
-                    <li><a href="produtos/soft.html"><ion-icon name="disc-sharp"></ion-icon> BISTEC SOFT</a></li>
-                    <li><a href="produtos/wifi.html"> <ion-icon name="wifi-sharp"></ion-icon> BISTEC WIFI</a></li>
+                    <li><a href="${Caminho}produtos/automacao.html"><ion-icon name="at-sharp"></ion-icon> BISTEC AUTOMAÇÃO</a></li>
+                    <li><a href="${Caminho}produtos/cloud.html"> <ion-icon name="cloud-done-sharp"></ion-icon> BISTEC CLOUD</a></li>
+                    <li><a href="${Caminho}produtos/edu.html"> <ion-icon name="pencil-sharp"></ion-icon> BISTEC EDU</a></li>
+                    <li><a href="${Caminho}produtos/gdpr.html"><ion-icon name="shield-checkmark-sharp"></ion-icon> BISTEC GDPR</a></li>
+                    <li><a href="${Caminho}produtos/msp.html"> <ion-icon name="lock-closed"></ion-icon> BISTEC MSP</a></li>
+                    <li><a href="${Caminho}produtos/outsourcing.html"> <ion-icon name="settings-sharp"></ion-icon> BISTEC OUTSOURCING</a></li>
+                    <li><a href="${Caminho}produtos/renting.html"> <ion-icon name="reload-circle-sharp"></ion-icon> BISTEC RENTING</a></li>
+                    <li><a href="${Caminho}produtos/soft.html"><ion-icon name="disc-sharp"></ion-icon> BISTEC SOFT</a></li>
+                    <li><a href="${Caminho}produtos/wifi.html"> <ion-icon name="wifi-sharp"></ion-icon> BISTEC WIFI</a></li>
                 </ul>
             </li>
             <li>
-                <a href="./contato.html" class="normal">CONTATO</a>
+                <a href="${Caminho}contato.html" class="normal">CONTATO</a>
             </li>
             <li>
                 <a href="#">
@@ -35,7 +36,7 @@ menu.insertAdjacentHTML('afterend',/*html*/`
                 </a>
             </li>
             <li>
-                <a href="suporte.html">
+                <a href="${Caminho}suporte.html">
                     <div class="button_support">
                         <span class="button_icon support_icon"><ion-icon name="headset-outline"></ion-icon></span>
                         <span class="button_text">SUPORTE</span>
