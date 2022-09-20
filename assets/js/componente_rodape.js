2d1a790452b7c6d7be60abe508db61ba11c26db7
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-09-20 14:53:10 -0300

    corrige erro de width e height não explicitos

--- assets/js/componente_rodape.js
+++ assets/js/componente_rodape.js
@@ -21,14 +21,14 @@ rodape.insertAdjacentHTML('afterend',/*html*/`
     <div class="contacts">
         <div class="contact item1">
             <a href="#first_section">
-                <img src="https://ik.imagekit.io/bistecpt/imagens/portugal_QoWCmflVD.png" alt="Portugal País"> Bistec Portugal
+                <img src="https://ik.imagekit.io/bistecpt/imagens/portugal_QoWCmflVD.png" alt="Portugal País"  width="20" height="20"> Bistec Portugal
             </a>
             <p><a href="tel:+351308807114">+351 308 807 114</a></p>
         </div>
 
         <div class="contact item2">
             <a href="https://bistec.com.br">
-                <img src="https://ik.imagekit.io/bistecpt/imagens/brazil_WcP0w91cK.png" alt="Brasil País"> Bistec Brasil 
+                <img src="https://ik.imagekit.io/bistecpt/imagens/brazil_WcP0w91cK.png" alt="Brasil País" width="20" height="20"> Bistec Brasil 
             </a>  
             <p>
                 <a href="tel:+55(13)3235-7735">+55 (13) 3235-7735</a>  
@@ -39,7 +39,8 @@ rodape.insertAdjacentHTML('afterend',/*html*/`
         </div>
 
         <div class="contact item3">
-            <a href="https://api.whatsapp.com/send?phone=5513992101945" target="_blank"><img src="https://ik.imagekit.io/bistecpt/imagens/whatsapp_qW0AZyxp4j.png" alt="Logo Whatsapp Business"> Whatsapp Business </a>
+            <a href="https://api.whatsapp.com/send?phone=5513992101945" target="_blank">
+            <img src="https://ik.imagekit.io/bistecpt/imagens/whatsapp_qW0AZyxp4j.png" alt="Logo Whatsapp Business" width="20" height="20"> Whatsapp Business</a>
             <p>
                 <a href="https://api.whatsapp.com/send?phone=5513992101945" target="_blank">+55 (13) 99210-1945</a>
             </p>
